64b2addcf380c273228526ca0cf5e171ce303177
mercenery <mercenery@inbox.ru>
2018-04-26 09:49:34 +0300

    spliterators.part1.exercises.unfaired.undone(Fedulov)

--- src/test/java/spliterators/exercise/exercise2/UnfairRectangleSpliterator.java
+++ src/test/java/spliterators/exercise/exercise2/UnfairRectangleSpliterator.java
@@ -4,7 +4,9 @@ import java.util.Spliterators;
 import java.util.function.IntConsumer;
 
 public class UnfairRectangleSpliterator extends Spliterators.AbstractIntSpliterator {
-
+    private int[][] doubleIntAr;
+    private int fromInclusive;
+    private int toExclusive;
     /**
      * 0 1 2 3 4
      * ---------
@@ -12,20 +14,34 @@ public class UnfairRectangleSpliterator extends Spliterators.AbstractIntSplitera
      * 2 4 5 6 7
      *
      * 0 1 2 3 4
-     * 2 3 / 4 5 6
+     * 2 3 4 5 6
      * 2 4 5 6 7
      */
     public UnfairRectangleSpliterator(int[][] data) {
-        this();
+        this(data, 0, data.length);
+        if (data.length == 0) {
+            throw new UnsupportedOperationException("Matrix passed is not rectangle !");
+        }
+        int secondDimensionSize = 0;
+        for (int[] aData : data) {
+            secondDimensionSize = 0;
+            for (int anAData : aData) {
+                secondDimensionSize++;
+            }
+            if (aData.length != secondDimensionSize) {
+                throw new UnsupportedOperationException("Matrix passed is not rectangle !");
+            }
+        }
     }
 
-    private UnfairRectangleSpliterator() {
-        super(0, 0);
-        throw new UnsupportedOperationException();
+    private UnfairRectangleSpliterator(int[][] data, fromInclusive, toExclusive) {
+        super(data.length, SIZED | IMMUTABLE | NONNULL | ORDERED);
+        this.doubleIntAr = data;
     }
 
     @Override
     public OfInt trySplit() {
+
         throw new UnsupportedOperationException();
     }
 
